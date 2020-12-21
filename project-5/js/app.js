0cb144c34aa52b79aa58316173453abbe00d5a4e
roberto <robpalacios1@hotmail.com>
2020-12-20 21:24:33 -0500

    add code JS to blur

--- project-5/js/app.js
+++ project-5/js/app.js
@@ -1,14 +1,23 @@
-const panels = document.querySelectorAll('.panel')
+const loadText = document.querySelector('.loading-text')
+const bg = document.querySelector('.bg')
 
-panels.forEach((panel) => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses();
-        panel.classList.add('active')
-    })
-})
+let load = 0;
 
-function removeActiveClasses() {
-    panels.forEach((panel) => {
-        panel.classList.remove('active')
-    })
+let int = setInterval(blurring, 30)
+
+
+function blurring() {
+    load++
+
+    if (load > 99) {
+        clearInterval(int)
+    }
+
+    loadText.innerText = `${load}%`
+    loadText.style.opacity = scale(load, 0, 100, 1, 0)
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
 }
